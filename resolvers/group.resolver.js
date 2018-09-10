ec49a3619f4332549c0631c30c5ab0a0cd9e1fd9
VK <vitaliy.krynytskyy@gmail.com>
2018-09-10 19:32:46 +0100

    Adds ability to create groups

--- resolvers/group.resolver.js
+++ resolvers/group.resolver.js
@@ -1,12 +1,21 @@
+import { formatError } from "graphql";
+import formatErrors from '../formatErrors';
+
 export default {
     Mutation: {
         createGroup: async (parent, args, { models, user }) => {
             try {
                 await models.Group.create({ ...args, owner: user.id });
-                return true;
+                return {
+                    ok: true,
+
+                };
             } catch (err) {
                 console.log(err);
-                return false;
+                return {
+                    ok: false,
+                    errors: formatErrors(err),
+                };
             }
         },
     }
