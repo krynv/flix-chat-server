345bef01aff919fd4d63b7bb6906c9e9efede0c5
VK <vitaliy.krynytskyy@gmail.com>
2019-06-25 18:22:03 +0100

    Pushing latest changes

--- server.js
+++ server.js
@@ -6,8 +6,11 @@ import path from 'path';
 import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
 
 import cors from 'cors';
+import jwt from 'jsonwebtoken';
 
 import models from './models';
+import { refreshTokens } from './auth';
+
 const SECRET = 'meme';
 const SECRET2 = 'ayyLmao';
 
@@ -25,17 +28,37 @@ const app = express();
 app.use(cors()); // enable cors
 app.use(bodyParser.json());
 
-app.use('/graphql', graphqlExpress({
+app.use('/graphql', graphqlExpress(req => ({
     schema,
     context: {
         models,
-        user: {
-            id: 2,
-        },
+        user: req.user,
         SECRET,
-        SECRET2
+        SECRET2,
+    },
+})),
+);
+
+const addUser = async (req, res, next) => {
+    const token = req.headers['x-token'];
+    if (token) {
+        try {
+            const { user } = jwt.verify(token, SECRET);
+            req.user = user;
+        } catch (err) {
+            const refreshToken = req / headers['x-refresh-token'];
+            const newTokens = await refreshTokens(token, refreshToken, models, SECRET, SECRET2);
+
+            if (newTokens.token && newTokens.refreshToken) {
+                res.set('Access-Control-Expose-Headers', 'x-token', 'x-refresh-token');
+                res.set('x-token', newTokens.token);
+                res.set('x-refresh-token', newTOkens.refreshToken);
+            }
+            req.user = newTokens.user;
+        }
     }
-}));
+    next();
+}
 
 app.use('/graphiql', graphiqlExpress({
     endpointURL: '/graphql'
